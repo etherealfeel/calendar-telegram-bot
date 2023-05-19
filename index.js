c6ab6b823800ba574e497c3de09431fd5cde629b
etherealfeel <graciagodsyndrome@gmail.com>
2023-05-19 18:06:52 +0300

    Configure jwt, add display events function

--- index.js
+++ index.js
@@ -1,28 +1,110 @@
+const { google } = require('googleapis');
 const TelegramBot = require('node-telegram-bot-api');
+const botToken = '6200492149:AAGtCvaqUGiXQA2USvS4OHUDutulQL0jz6o';
+require('dotenv').config();
 
+//Create telegram bot
+const bot = new TelegramBot(botToken, { polling: true });
 
-const token = '6200492149:AAGtCvaqUGiXQA2USvS4OHUDutulQL0jz6o'
+// Provide the required configuration
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+const credentialsJSON = require('./credentials.json');
+const calendarId = process.env.CALENDAR_ID;
 
-const bot = new TelegramBot(token, {polling: true});
+// Google calendar API settings
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
+// const calendar = google.calendar({ version: 'v3', auth: CREDENTIALS });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
+const jwtClient = new google.auth.JWT(
+  CREDENTIALS.client_email,
+  null,
+  CREDENTIALS.private_key,
+  SCOPES
+);
+
+const calendar = google.calendar({
+  version: "v3",
+  auth: jwtClient,
+});
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "./credentials.json",
+  scopes: SCOPES,
+});
+
+async function createEvent(title, dateTime) {
+  const event = {
+    summary: title,
+    start: {
+      dateTime: dateTime.toISOString(),
+      timeZone: 'America/New_York',
+    },
+    end: {
+      dateTime: dateTime.toISOString(),
+      timeZone: 'America/New_York',
+    },
+  };
+
+  return calendar.events.insert({
+    calendarId: calendarId,
+    auth: jwtClient,
+    resource: event,
   });
-  
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-  
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+}
+
+let eventTitle = '';
+let eventDateTime = '';
+
+bot.onText(/\/add/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Please enter the event title:');
+  bot.onText(/(.+)/, (titleMsg, titleMatch) => {
+    if (titleMsg.chat.id === chatId) {
+      eventTitle = titleMatch[1];
+
+      bot.sendMessage(chatId, 'Please enter the event date and time (YYYY-MM-DD HH:mm):');
+      bot.onText(/(.+)/, (dateMsg, dateMatch) => {
+        if (dateMsg.chat.id === chatId) {
+          eventDateTime = new Date(dateMatch[1]);
+
+          createEvent(eventTitle, eventDateTime)
+            .then(() => {
+              bot.sendMessage(chatId, 'Event created successfully!');
+            })
+            .catch((err) => {
+              console.error('Error creating event:', err);
+              bot.sendMessage(chatId, 'Error creating event. Please try again later.');
+            });
+        }
+      });
+    }
   });
+});
+
+bot.onText(/\/events/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  try {
+    const response = await calendar.events.list({
+      calendarId: process.env.CALENDAR_ID,
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+
+    const events = response.data.items;
+    let message = 'Upcoming events:\n';
+    events.forEach((event) => {
+      const start = event.start.dateTime || event.start.date;
+      message += `${start} - ${event.summary}\n`;
+    });
+
+    bot.sendMessage(chatId, message);
+  } catch (err) {
+    console.error('Error retrieving events:', err);
+    bot.sendMessage(chatId, 'Error retrieving events. Please try again later.');
+  }
+
+});
